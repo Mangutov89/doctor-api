2e2e356e0698e7af558cfd0fdfb11046ce8ebec2
Student <example@example.com>
2019-03-11 16:06:00 -0700

    add dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './sass/styles.scss';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,44 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-
-
   devtool: 'eval-source-map',
   devServer: {
     contentBase: './dist'
   },
-
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Triangle Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
-
     rules: [
-      // configurations for loaders will go here!
-
       {
-        test: /\.scss$/,
+        test: /\.css$/,
         use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
+          'style-loader',
+          'css-loader'
         ]
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [
+                /node_modules/,
+                /spec/
+              ],
         loader: "eslint-loader"
       },
-      {
-        test: /\.(gif|png|jpe?g)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[name].[ext]',
-                outputPath: 'assets/images/'
-              }
-            }
-          ]
-        },
-
-        {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      },
-
     ]
-  },
-
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './src/index.html',
-      filename: 'index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      }
-    }),
-
-    new HtmlWebpackPlugin({
-      template: './src/example.html',
-      filename: 'example.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      }
-    }),
-    new UglifyJsPlugin(),
-
-    new CleanWebpackPlugin(['dist'])
-  ]
-
+  }
 };
