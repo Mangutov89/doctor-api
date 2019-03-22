59872c53dafb22f00ebcf97594b0358fef099220
lm <leo.mangutov@gmail.com>
2019-03-22 16:34:40 -0700

    Add results to show doctor and loop thru the doctors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 
-import { GetSymptom } from './../src/api.js';
+import { GetMedicalForm } from './../src/api.js';
 
 $(document).ready(function() {
   $("#medical-form").submit(function(event) {
@@ -11,14 +11,19 @@ $(document).ready(function() {
   const symptomInput = $("#symptom").val();
   $("#symptom").val("");
 
-    let newSymptom = new GetSymptom();
-    let promise = newSymptom.getDoctors(symptomInput);
+    let newSymptom = new GetMedicalForm();
+    let promise = newSymptom.getSymptom(symptomInput);
 
     promise.then(function(response) {
        let body =JSON.parse(response);
-       console.log(body.data);
-
-       $('#results-bio').text(`Learn more about this artist: ${body}`);
+       console.log(body.data[0].profile.image_url);
+       if (body.data.length < 0) {
+         $('#results').text("We could no find a doctor in your area to match those symptoms");
+       } else {
+          for (var i = 0 ; i < 20 ; i ++) {
+            $("#results").html(`here is a picture <img src="${body.data[i].profile.image_url}">`)
+         }
+       }
 
     }, function(error) {
        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
