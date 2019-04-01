d4c7b40073dfdc9b5ad04923c89aede7fa9abab4
lm <leo.mangutov@gmail.com>
2019-04-01 13:54:19 -0700

    Add doctor name to search results

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export class GetMedicalForm {
-  getSymptom(symptom) {
+  getSymptom(doctorLast, symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&sort=best-match-asc&skip=0&limit=20&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${doctorLast}&query=${symptom}&location=or-portland&sort=best-match-asc&skip=0&limit=20&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/index.js
+++ src/index.js
@@ -9,19 +9,21 @@ $(document).ready(function() {
   $("#medical-form").submit(function(event) {
   event.preventDefault();
   const symptomInput = $("#symptom").val();
+  const doctorLast = $("#doctorLast").val();
   $("#symptom").val("");
+  $("#doctorLast").val("");
 
     let newSymptom = new GetMedicalForm();
-    let promise = newSymptom.getSymptom(symptomInput);
+    let promise = newSymptom.getSymptom(doctorLast, symptomInput);
 
     promise.then(function(response) {
        let body =JSON.parse(response);
-       console.log(body.data[0].profile.image_url);
-       if (body.data.length < 0) {
+       console.log(body.data);
+       if (body.data.length === 0) {
          $('#results').text("We could no find a doctor in your area to match those symptoms");
        } else {
           for (var i = 0 ; i < 20 ; i ++) {
-            $("#results").html(`here is a picture <img src="${body.data[i].profile.image_url}">`)
+            $("#results").append(`Name: ${body.data[i].profile.first_name} ${body.data[i].profile.last_name} <br> Street: ${body.data[i].practices[0].visit_address.street} <br> ${body.data[i].practices[0].visit_address.city}, ${body.data[i].practices[0].visit_address.state} ${body.data[i].practices[0].visit_address.zip} <br> Phone Number: ${body.data[i].practices[0].phones[0].number} <br> <br>`);
          }
        }
 
